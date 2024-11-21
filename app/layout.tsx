b042321b31d7582fd0968fbf07520ecb884c7cb7
Sepide Kia <sepidekia6990@gmail.com>
2024-11-21 13:18:22 +0330

    feat(fetch in server component) test the fetching in server components with jsonplaceholder api

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <header>
-        <Logo />
-        <Navigation />
-      </header>
       <body>
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
         <main>{children}</main>
         <footer>Copyright by Septel</footer>
       </body>
--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <div>rooms</div>;
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data, "this will be shown in the node runtime");
+
+  return (
+    <ul>
+      {data.map(({ id, name }: { id: string; name: string }) => (
+        <li key={id}>{name}</li>
+      ))}
+    </ul>
+  );
 }
