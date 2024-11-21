dbe6b789f2f7301c244b51a250203bf11ea5eac9
Sepide Kia <sepidekia6990@gmail.com>
2024-11-21 21:36:52 +0330

    feat(interactivity): check interactivitiy in a client component

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,13 +1,20 @@
+import Counter from "../_components/Counter";
+
+export type User = { id: string; name: string };
+
 export default async function Page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
+  const data = (await res.json()) as User[];
   console.log(data, "this will be shown in the node runtime");
 
   return (
-    <ul>
-      {data.map(({ id, name }: { id: string; name: string }) => (
-        <li key={id}>{name}</li>
-      ))}
-    </ul>
+    <div>
+      <Counter users={data} />
+      <ul>
+        {data.map(({ id, name }) => (
+          <li key={id}>{name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
