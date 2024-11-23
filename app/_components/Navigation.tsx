b34a6916cef4289e9afc10e26e10bdd5c8d3f6c7
Sepide Kia <sepidekia6990@gmail.com>
2024-11-23 10:28:35 +0330

    feat(active route)  add style to the active rout

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,22 +1,31 @@
+"use client";
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const navigations = [
+const links = [
   { href: "/", label: "Home" },
   { href: "/rooms", label: "Rooms" },
   { href: "/about", label: "About" },
   { href: "/account", label: "Your Account" },
 ];
 export default function Navigation() {
+  const currentPath = usePathname();
+
   return (
     <nav className="z-10 text-xl">
-      <ul className="flex gap-16 items-center">
-        {navigations.map((item) => (
-          <li key={item.label}>
+      <ul className="flex gap-16 links-center">
+        {links.map((link) => (
+          <li key={link.label}>
             <Link
-              href={item.href}
-              className="hover:text-accent-400 transition-colors"
+              href={link.href}
+              className={classNames({
+                "text-zinc-500": currentPath !== link.href,
+                "text-accent-400": currentPath === link.href,
+                "hover:text-accent-400 transition-colors": true,
+              })}
             >
-              {item.label}
+              {link.label}
             </Link>
           </li>
         ))}
