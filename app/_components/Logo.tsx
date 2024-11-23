8a05af77f3a7783f92be866d61d34a450aa09941
Sepide Kia <sepidekia6990@gmail.com>
2024-11-23 10:00:22 +0330

    feat(about) build the about page

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
       <img src="/logo.png" height="60" width="60" alt="Septel" />
-      <span className="text-xl font-semibold text-primary-100">Septel</span>
+      <span className="text-xl font-semibold text-primary-600">Septel</span>
     </Link>
   );
 }
