9d8d36d066124e2775c30207ea7599f9b6f87bb7
Sepide Kia <sepidekia6990@gmail.com>
2024-11-26 09:38:33 +0330

    feat(rooms api) create delete and put api with its validation for rooms

--- app/api/rooms/route.tsx
+++ app/api/rooms/route.tsx
@@ -1,35 +1,43 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
 
 //prevent cachting
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function GET(request: NextRequest) {
   return NextResponse.json([
-    // {
-    //   id: 1,
-    //   created_at: "2024-11-25 16:01:22.9547+00",
-    //   name: "room_1",
-    //   maxCapacity: 3,
-    //   regularPrice: 1000,
-    //   discount: 60,
-    //   description: "sea side",
-    //   image: "",
-    // },
-    // {
-    //   id: 2,
-    //   created_at: "2024-11-25 20:01:22.9547+00",
-    //   name: "room_2",
-    //   maxCapacity: 2,
-    //   regularPrice: 700,
-    //   discount: 40,
-    //   description: "sea side",
-    //   image: "",
-    // },
-    { name: "Sepid", id: 1 },
-    { name: "Narge", id: 2 },
+    {
+      id: 1,
+      created_at: "2024-11-25 16:01:22.9547+00",
+      name: "room_1",
+      maxCapacity: 3,
+      regularPrice: 1000,
+      discount: 60,
+      description: "sea side",
+      image: "",
+    },
+    {
+      id: 2,
+      created_at: "2024-11-25 20:01:22.9547+00",
+      name: "room_2",
+      maxCapacity: 2,
+      regularPrice: 700,
+      discount: 40,
+      description: "sea side",
+      image: "",
+    },
   ]);
 }
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  return NextResponse.json(body);
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  if (!body.name)
+    return NextResponse.json(
+      { error: "The room must have name." },
+      { status: 404 }
+    );
+  return NextResponse.json({ id: 3, ...body });
 }
---
+++ app/api/rooms/schema.tsx
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().min(1),
+  maxCapacity: z.number().int().positive().optional(),
+  regularPrice: z.number().positive().optional(),
+  discount: z.number().min(0).max(100).optional(),
+  description: z.string().optional(),
+  image: z.string().optional().nullable(),
+});
+
+export default schema;
