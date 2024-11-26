b5495e5e2f6bc7d0dcfb813ccaefedd85e0ae7fa
Sepide Kia <sepidekia6990@gmail.com>
2024-11-26 21:59:02 +0330

    refactor(prisma usage) use prisma in the get room and get rooms APIs

--- app/api/rooms/route.tsx
+++ app/api/rooms/route.tsx
@@ -1,31 +1,12 @@
+import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 
 //prevent cachting
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    {
-      id: 1,
-      created_at: "2024-11-25 16:01:22.9547+00",
-      name: "room_1",
-      maxCapacity: 3,
-      regularPrice: 1000,
-      discount: 60,
-      description: "sea side",
-      image: "",
-    },
-    {
-      id: 2,
-      created_at: "2024-11-25 20:01:22.9547+00",
-      name: "room_2",
-      maxCapacity: 2,
-      regularPrice: 700,
-      discount: 40,
-      description: "sea side",
-      image: "",
-    },
-  ]);
+export async function GET(request: NextRequest) {
+  const rooms = await prisma.room.findMany();
+  return NextResponse.json(rooms);
 }
 
 export async function POST(request: NextRequest) {
