a4bbd3c02ee9b3f1a74f601b8774a4a9210ed5d1
Sepide Kia <sepidekia6990@gmail.com>
2024-11-29 01:16:46 +0330

    refactor(suspense usage) use suspens for rooms list

---
+++ app/_components/RoomsList.tsx
@@ -0,0 +1,19 @@
+import { Room } from "@/_types/index";
+import RoomCard from "./RoomCard";
+
+export default async function RoomsList() {
+  //TODO: change to db
+  const res = await fetch("http://localhost:3000/api/rooms", {
+    cache: "no-cache",
+  });
+  const rooms = (await res.json()) as Room[];
+
+  if (!rooms.length) return null;
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+      {rooms.map((room) => (
+        <RoomCard room={room} key={room.id} />
+      ))}
+    </div>
+  );
+}
