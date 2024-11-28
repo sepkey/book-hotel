9663bdee0f0aecc3299f46e163b1f7f57f676a4d
Sepide Kia <sepidekia6990@gmail.com>
2024-11-29 01:37:23 +0330

    feat(room page) add room page component

--- app/rooms/loading.tsx
+++ app/rooms/loading.tsx
@@ -4,7 +4,7 @@ export default function loading() {
   return (
     <div className="grid justify-center items-center">
       <Spinner />
-      <p className="text-primary-500">Loading rooms...</p>
+      <p className="text-primary-500">Loading room data...</p>
     </div>
   );
 }
